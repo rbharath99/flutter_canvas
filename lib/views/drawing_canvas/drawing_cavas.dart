82891ab1fde54a31f968210c7a8b67c4177be2f7
Ryan Bharath <89420193+rbharath99@users.noreply.github.com>
2023-12-05 11:45:57 -0500

    Feat/draw on cavnas (#4)
    * feat: able to draw on cavas
    
    * feat: added precise mouse cursor to easily track it on the canvas
    
    * fix: adjusted stroke width for the brush

--- lib/views/drawing_canvas/drawing_cavas.dart
+++ lib/views/drawing_canvas/drawing_cavas.dart
@@ -1,28 +1,64 @@
 import 'package:flutter/material.dart';
 
-class DrawingCanvas extends StatelessWidget {
+class DrawingCanvas extends StatefulWidget {
   const DrawingCanvas({super.key});
 
   @override
+  State<DrawingCanvas> createState() => _DrawingCanvasState();
+}
+
+class _DrawingCanvasState extends State<DrawingCanvas> {
+  final List<Offset> offsets = [];
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.maxFinite,
-      height: double.maxFinite,
-      child: CustomPaint(
-        painter: SketchPainter(),
+    return MouseRegion(
+      cursor: SystemMouseCursors.precise,
+      child: GestureDetector(
+        onPanUpdate: (DragUpdateDetails details) {
+          setState(() {
+            offsets.add(details.globalPosition);
+          });
+        },
+        onPanEnd: (DragEndDetails details) => setState(() {}),
+        child: RepaintBoundary(
+          child: SizedBox(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: CustomPaint(
+              painter: SketchPainter(offsets: offsets),
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
 class SketchPainter extends CustomPainter {
+  const SketchPainter({required this.offsets});
+
+  final List<Offset> offsets;
+
   @override
   void paint(Canvas canvas, Size size) {
-
+    final paint = Paint();
+    paint.color = Colors.blue;
+    paint.strokeCap = StrokeCap.round;
+    paint.strokeWidth = 20;
+    for (int i = 0; i < offsets.length - 1; i++) {
+      final path = Path();
+      path.moveTo(offsets[i].dx, offsets[i].dy);
+      path.quadraticBezierTo(
+        offsets[i].dx,
+        offsets[i].dy,
+        (offsets[i].dx + offsets[i + 1].dx) / 2,
+        (offsets[i].dy + offsets[i + 1].dy) / 2,
+      );
+      canvas.drawPath(path, paint);
+    }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
