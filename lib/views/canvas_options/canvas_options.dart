5b19fc7e52cc577d49e6beb6a1a0a3f402613e20
rbharath99 <dbossman60@gmail.com>
2023-12-04 23:18:13 -0500

    feat: showed color picker on the drawer

--- lib/views/canvas_options/canvas_options.dart
+++ lib/views/canvas_options/canvas_options.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_canvas/views/canvas_options/widgets/color_selector.dart';
 
 class CanvasOptions extends StatelessWidget {
   const CanvasOptions({super.key});
@@ -14,7 +15,7 @@ class CanvasOptions extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Placeholder(),
-          Placeholder(),
+          ColorSelector(),
           Placeholder(),
         ],
       ),
