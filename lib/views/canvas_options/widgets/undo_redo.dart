6e085e4856420d14d9344187f7110b0c5535d536
rbharath99 <dbossman60@gmail.com>
2023-12-07 11:32:49 -0500

    style: completed styling for the undo redo ui

--- lib/views/canvas_options/widgets/undo_redo.dart
+++ lib/views/canvas_options/widgets/undo_redo.dart
@@ -16,19 +16,38 @@ class UndoRedo extends StatelessWidget {
             Tooltip(
               message: 'Undo',
               child: IconButton(
+                color: Colors.black,
                 icon: const Icon(Icons.undo),
                 onPressed: () {
                   context.read<CanvasBloc>().add(Undo());
                 },
+                style: IconButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  side: const BorderSide(
+                    width: 2,
+                  ),
+                ),
               ),
             ),
+            const SizedBox(width: 10),
             Tooltip(
               message: 'Redo',
               child: IconButton(
+                color: Colors.black,
                 icon: const Icon(Icons.redo),
                 onPressed: () {
                   context.read<CanvasBloc>().add(Redo());
                 },
+                style: IconButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  side: const BorderSide(
+                    width: 2,
+                  ),
+                ),
               ),
             ),
           ],
