a7250967bc3423ebc85d117718bb82e0bf2fcc09
rbharath99 <dbossman60@gmail.com>
2023-12-08 12:41:43 -0500

    chore: working on cursor for canvas tools

--- lib/views/drawing_canvas/drawing_cavas.dart
+++ lib/views/drawing_canvas/drawing_cavas.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_canvas/views/drawing_canvas/widgets/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DrawingCanvas extends StatefulWidget {
   const DrawingCanvas({super.key});
@@ -9,15 +10,46 @@ class DrawingCanvas extends StatefulWidget {
 }
 
 class _DrawingCanvasState extends State<DrawingCanvas> {
+  Offset pointer = Offset.zero;
   @override
   Widget build(BuildContext context) {
-    return const MouseRegion(
-      cursor: SystemMouseCursors.precise,
-      child: Stack(
-        children: [
-          AllSketches(),
-          CurrentSketch(),
-        ],
+    return MouseRegion(
+      cursor: SystemMouseCursors.none,
+      child: Listener(
+        onPointerHover: (details) {
+          setState(() {
+            pointer = details.position;
+          });
+        },
+        onPointerDown: (details) {
+          setState(() {
+            pointer = details.position;
+          });
+        },
+        onPointerMove: (details) {
+          setState(() {
+            pointer = details.position;
+          });
+        },
+        onPointerUp: (details) {
+          setState(() {
+            pointer = details.position;
+          });
+        },
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              duration: const Duration(
+                milliseconds: 5,
+              ),
+              left: pointer.dx,
+              top: pointer.dy,
+              child: const Icon(FontAwesomeIcons.pen),
+            ),
+            const AllSketches(),
+            const CurrentSketch(),
+          ],
+        ),
       ),
     );
   }
