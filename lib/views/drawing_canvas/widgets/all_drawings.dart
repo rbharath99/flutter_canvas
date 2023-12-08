64f213486b5d201c8206a0c193ed95e2548f6f2d
rbharath99 <dbossman60@gmail.com>
2023-12-08 15:49:09 -0500

    refactor: converted AllSketches into a stateless widget

--- lib/views/drawing_canvas/widgets/all_drawings.dart
+++ lib/views/drawing_canvas/widgets/all_drawings.dart
@@ -3,14 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_canvas/utils/sketch_painter.dart';
 import 'package:flutter_canvas/views/drawing_canvas/bloc/canvas_bloc.dart';
 
-class AllSketches extends StatefulWidget {
+class AllSketches extends StatelessWidget {
   const AllSketches({super.key});
 
-  @override
-  State<AllSketches> createState() => _AllSketchesState();
-}
-
-class _AllSketchesState extends State<AllSketches> {
   @override
   Widget build(BuildContext context) {
     final allDrawings =
